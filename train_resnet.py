95ea329fe315655cbb81abee02ea4a6b42a91f69
KyungMinJin <kyung6479@korea.ac.kr>
2021-09-10 17:41:47 +0900

    resnet v2

---
+++ train_resnet.py
@@ -0,0 +1,142 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import CIFAR10
+from torch.utils.data import  DataLoader
+
+from utils.model.ResNet import resnet
+import  argparse
+from tensorboardX import SummaryWriter
+
+parser = argparse.ArgumentParser(description='cifar10 cls model')
+parser.add_argument('--lr', default=0.1)
+parser.add_argument('--resume', default=None)
+parser.add_argument('--batch_size', default=128)
+parser.add_argument('--batch_size_test', default=100)
+parser.add_argument('--num_worker', default=4)
+parser.add_argument('--logdir', default='logs')
+args = parser.parse_args()
+
+transforms_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+transforms_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+dataset_train = CIFAR10(root='./data', train=True, download=False, transform=transforms_train)
+dataset_test = CIFAR10(root='./data', train=False, download=False, transform=transforms_test)
+
+train_loader = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, num_workers=args.num_worker)
+test_loader = DataLoader(dataset_test, batch_size=args.batch_size, shuffle=False, num_workers=args.num_worker)
+
+model = resnet().to('cuda')
+num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+if args.resume is not None:
+    checkpoints = torch.load('./checkpoints/' + args.resume)
+    model.load_state_dict(checkpoints['model'])
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+
+decay_epoch = [32000, 48000]
+step_lr_scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=decay_epoch, gamma=0.1)
+writer = SummaryWriter(args.logdir)
+
+def train(epoch, global_steps):
+    model.train()
+
+    train_loss = 0
+    correct = 0
+    total = 0
+
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        global_steps += 1
+        step_lr_scheduler.step()
+        inputs = inputs.to('cuda')
+        targets = targets.to('cuda')
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        acc = 100 * correct / total
+        print(f'train epoch : {epoch} [{batch_idx} / {len(train_loader)}] | loss: {train_loss/ (batch_idx + 1):.3f} | acc: {acc}')
+
+        writer.add_scalar('log/train error', 100 - acc, global_steps)
+    return global_steps
+
+def test(epoch, best_acc, global_steps):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(test_loader):
+            inputs = inputs.to('cuda')
+            targets = targets.to('cuda')
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            acc = 100 * correct / total
+            print(
+            f'test epoch : {epoch} [{batch_idx} / {len(test_loader)}] | loss: {test_loss / (batch_idx + 1):.3f} | acc: {acc}')
+
+            writer.add_scalar('log/test error', 100 - acc, global_steps)
+
+    if acc > best_acc:
+        print('==> Saving model')
+        state = {
+            'model': model.state_dict(),
+            'acc': acc,
+            'epoch': epoch
+        }
+        if not os.path.isdir('checkpoints'):
+            os.mkdir('checkpoints')
+        torch.save(state, './checkpoints/ckpt.pth')
+        best_acc = acc
+
+    return best_acc
+
+if __name__ == '__main__':
+    best_acc = 0
+    epoch = 0
+    global_steps = 0
+
+    if args.resume is not None:
+        test(epoch=0, best_acc=0)
+    else:
+        while True:
+            epoch += 1
+            global_steps = train(epoch, global_steps)
+            best_acc = test(epoch, best_acc, global_steps)
+            print('best test accuracy is', best_acc)
+
+            if global_steps >= 64000:
+                break
--- utils/model/ResNet.py
+++ utils/model/ResNet.py
@@ -2,30 +2,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False)
+# def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+#     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False)
+#
+#
+# def conv1x1(in_planes, out_planes, stride=1):
+#     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
-
-def conv1x1(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-class BasicBlock(nn.Module):
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, dilation=1, norm_layer=None):
-        super(BasicBlock, self).__init__()
-
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, down_sample=False):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
+
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         self.stride = stride
 
+        if down_sample:
+            self.down_sample = IdentityPadding(in_channels, out_channels, stride)
+        else:
+            self.down_sample = None
+
     def forward(self, x):
-        identity = x
+        shortcut = x
 
         out = self.conv1(x)
         out = self.bn1(out)
@@ -35,69 +36,87 @@ class BasicBlock(nn.Module):
         out = self.bn2(out)
 
         if self.downsample is not None:
-            identity = self.downsample(x)
+            shortcut = self.down_sample(x)
 
-        out += identity     # residual connection
+        out += shortcut     # residual connection
         out = self.relu(out)
+        return out
 
+
+# zero pad 수행, feature map 의 마지막 축에대해 (0, 0) 으로 패딩, 마지막에서 두번째 축에 (0, 0),
+# 세 번째 축에대해 (0, self.add_channels)만큼 패딩
+# channel 축에 대해 한 방향으로 self.add_channels 만큼 패딩을 주어 같은 크기로 바뀜
+class IdentityPadding(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super(IdentityPadding, self).__init__()
+
+        self.pooling = nn.MaxPool2d(1, stride=stride)
+        self.add_channels = out_channels - in_channels
+
+    def forward(self, x):
+        out = F.pad(x, (0, 0, 0, 0, 0, self.add_channels))
+        out = self.pooling(out)
         return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False, norm_layer=None):
+    def __init__(self, num_layers, block, num_classes=10):
         super(ResNet, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self.norm_layer = norm_layer    # batch norm layer
-
-        self.inplanes = 64   # input shape
-        self.dilation = 1   # dilation fixed
-        self.groups = 1     # groups fixed
-
-        # input block
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = norm_layer(self.inplanes)
+        self.num_layers = num_layers
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        # residual blocks
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=False)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilate=False)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilate=False)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512, num_classes)
+        # feature map 32x32x16
+        self.layer_2n = self.get_layers(block, 16, 16, stride=1)
+        # feature map 16x16x32
+        self.layer_4n = self.get_layers(block, 16, 32, stride=2)
+        # feature map 8x8x64
+        self.layer_6n = self.get_layers(block, 32, 64, stride=2)
+
+        # output layers
+        self.avg_pool = nn.AvgPool2d(8, stride=1)
+        self.fc_out = nn.Linear(64, num_classes)
 
         # weight init
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+            elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.weight, 0)
 
-        # if zero_init_residual:
-        #     for m in self.modules():
-        #         if isinstance(m, BottleNeck):
+    def get_layers(self, block, in_channels, out_channels, stride):
+        if stride == 2:
+            down_sample = True
+        else:
+            down_sample = False
+
+        layers_list = nn.ModuleList(
+            [block(in_channels, out_channels, stride, down_sample)]
+        )
+
+        for _ in range(self.num_layers - 1):
+            layers_list.append(block(out_channels, out_channels))
+
+        return nn.Sequential(*layers_list)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer_2n(x)
+        x = self.layer_4n(x)
+        x = self.layer_6n(x)
+
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_out(x)
+        return x
 
 
-    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
-        norm_layer = self._norm_layer
-        downsample = None
-
-        if stride != 1 or self.inplanes != planes:
-            downsample = nn.Sequential(conv1x1(self.inplanes, planes, stride), norm_layer(planes))
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, self.groups, self.dilation, norm_layer))
-        self.inplanes = planes
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, groups=self.groups, dilation=self.dilation, norm_layer=norm_layer))
-
-        return nn.Sequential(*layers)
-
-
-
-
-
-
+def resnet():
+    block = ResidualBlock
+    model = ResNet(5, block)
+    return model
